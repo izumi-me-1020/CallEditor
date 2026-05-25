e8399adc41cbddff4a8a00c02687c81046f61add
izumi-me-1020 <izumi@izumy.me>
2026-05-26 01:37:47 +0900

    update

--- src/seo/analytics.tsx
+++ src/seo/analytics.tsx
@@ -1,12 +1,15 @@
 import { Head } from "vite-react-ssg";
 
-const GA4_ID = "G-RP0FV0JJ9P";
+const GA4_ID = "G-G7W3J8HYQ5";
 
 const AnalyticsScripts: React.FC = () => {
   if (!GA4_ID) return null;
   return (
     <Head>
-      <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA4_ID}`} />
+      <script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA4_ID}`}
+      />
       <script>
         {`window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
